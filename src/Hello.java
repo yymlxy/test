67f9e6559a0892b72fba9fa5ee811c882c7d7b1e
yymlxy <358285597@qq.com>
2021-01-18 10:49:32 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * 一个类
+ * @author  YYM
+ */
+public class Hello {
+    /**
+     * 主方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hellossssssssssssssssssssssd中国ssssssssssssssssssssssssssssssssssaadf");
+    }
+}
