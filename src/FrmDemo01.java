f8e4719dd49876cfa74829b248c45babb4f4400e
yymlxy <358285597@qq.com>
2021-01-18 10:56:21 +0800

    增加一个窗体程序

---
+++ src/FrmDemo01.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class FrmDemo01 {
+    public static void main(String[] args) {
+        JFrame frm=new JFrame();
+        frm.setSize(300,200);
+        frm.setLocationRelativeTo(null);
+        frm.setVisible(true);
+    }
+}
